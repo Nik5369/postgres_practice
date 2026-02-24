2e49da7431e54f3bcef6e83b6accd702fe2d4d30
Nikita <natsu25369@gmail.com>
2026-02-24 20:35:17 +0300

    feature/add_postgres_connection

---
+++ feature_postgres/simple_connection/simple_connection.go
@@ -0,0 +1,26 @@
+package simple_connection
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+//"postgres://YourUserName:YourPassword@YouHostName:5432/YouDatabaseName"
+
+
+func CheckConnection() {
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, "postgres://postgres:d310301@localhost:5432/postgres")	
+	if err != nil {
+		panic(err)
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connection success")
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "postgres_connection/feature_postgres/simple_connection"
+
 func main() {
-	
+	simple_connection.CheckConnection()	
 }
